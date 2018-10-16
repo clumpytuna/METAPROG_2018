9ff9a48d150973ab817317e904da23b919f0f17e
Clumpy Tuna <anuhinm@mail.ru>
2018-10-16 23:04:07 +0300

    Initial Commit

---
+++ MIPT_METAPROG_2018/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  MIPT_METAPROG_2018
+//
+//  Created by Clumpy Tuna on 16.10.2018.
+//  Copyright © 2018 Clumpy Tuna. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+	return 0;
+}
